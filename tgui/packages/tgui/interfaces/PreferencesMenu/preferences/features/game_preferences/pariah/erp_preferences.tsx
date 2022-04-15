20181f94b37a07f780d2aa8e1eb19fd970aa012f
ElorgRHG <71735193+ElorgRHG@users.noreply.github.com>
2022-04-16 01:37:03 +0200

    Master ERP pref no longer shows up as error (#405)

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/pariah/erp_preferences.tsx
@@ -0,0 +1,8 @@
+import { CheckboxInput, FeatureToggle } from "../../base";
+
+export const master_erp_pref: FeatureToggle = {
+  name: "Show/Hide Erotic Roleplay Preferences",
+  category: "ERP",
+  description: "This shows/hides ERP preferences.",
+  component: CheckboxInput,
+};

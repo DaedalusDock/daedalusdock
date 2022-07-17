HolyTigerGod/Voidcrew

Confusion status effect is now duration based instead of magic number based (#66801)
Refactors the confusion status effect. Removes "confusion strength" and replaces it with duration, which is measured in seconds.
This also allows them to use the adjust_timed_status_effect procs instead of their own.

Fun fact! 2 years ago when confusion was refactored into status effects, all confusion effects in the game were halved in duration. They were changed to status effects, which tick every 1 second by default, from life, which tick every 2 seconds by default, without any values changing.

code/modules/antagonists/abductor/equipment/glands/mindshock.dm
@@ -25,7 +25,7 @@
 				target.Stun(50)
 			if(2)
 				to_chat(target, span_warning("You hear an annoying buzz in your head."))
-				target.add_confusion(15)
+				target.adjust_timed_status_effect(15 SECONDS, /datum/status_effect/confusion)
 				target.adjustOrganLoss(ORGAN_SLOT_BRAIN, 10, 160)
 			if(3)
 				target.hallucination += 60

DaedalusDock/daedalusdock

code/modules/antagonists/abductor/equipment/glands/mindshock.dm
@@ -20,8 +20,8 @@
 				to_chat(H, span_userdanger("You hear a loud buzz in your head, silencing your thoughts!"))
 				H.Stun(50)
 			if(2)
-				to_chat(H, span_warning("You hear an annoying buzz in your head."))
-				H.add_confusion(15)
+				to_chat(target, span_warning("You hear an annoying buzz in your head."))
+				H.adjust_timed_status_effect(15 SECONDS, /datum/status_effect/confusion)
 				H.adjustOrganLoss(ORGAN_SLOT_BRAIN, 10, 160)
 			if(3)
 				H.hallucination += 60
2006fa2b9de7677ef62e7ae26cc7d43051225cbb
Jeremiah <42397676+jlsnow301@users.noreply.github.com>
2024-06-04 22:47:44 -0700

    tgui: dependency updates (#971)
    * node&yarn
    
    * module updates
    
    * sonarjs
    
    * eslint&prettier
    
    * highest TS we can use
    
    * built in now
    
    * reeeee
    
    * added required dep

--- dependencies.sh
+++ dependencies.sh
@@ -11,8 +11,8 @@ export BYOND_MINOR=1630
 export RUST_G_VERSION=3.1.0
 
 #node version
-export NODE_VERSION=14
-export NODE_VERSION_PRECISE=14.16.1
+export NODE_VERSION=20
+export NODE_VERSION_PRECISE=20.14.0
 
 # SpacemanDMM git tag
 export SPACEMAN_DMM_VERSION=suite-1.8
